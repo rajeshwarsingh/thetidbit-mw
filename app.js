706dee32a22f18e6bd63e51db7e695de0557d942
unknown <rajan23024@gmail.com>
2022-03-16 16:46:33 +0530

    added cores

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(cors({
-  origin: ['https://thetidbit.in', 'https://thetidbit.in/','https://www.thetidbit.in/','http://www.thetidbit.in/','http://thetidbit.in/']
+  origin: ['https://thetidbit.in', 'https://thetidbit.in/','https://www.thetidbit.in/','http://www.thetidbit.in/','http://thetidbit.in/','http://localhost:3000','http://localhost:3000/']
 }));
 app.use(logger('dev'));
 app.use(express.json());
