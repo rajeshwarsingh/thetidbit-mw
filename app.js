05580166a4ad241ccfbb45d8dcd796a12d01c43a
unknown <rajan23024@gmail.com>
2022-03-16 15:09:54 +0530

    added cores for local

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
